85f80d632428beb8a030cb188624e7c7d01df955
sbartkowiak <szymon.b4310@gmail.com>
2025-10-31 11:23:58 +0100

    init

---
+++ src/main/java/com/example/hellowordcicd/HelloWordCiCdApplication.java
@@ -0,0 +1,13 @@
+package com.example.hellowordcicd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWordCiCdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloWordCiCdApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/hellowordcicd/HelloWordCiCdApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.hellowordcicd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@SpringBootTest
+class HelloWordCiCdApplicationTests {
+
+    @Test
+    void contextLoads() {
+        assertTrue(true);
+    }
+
+}
